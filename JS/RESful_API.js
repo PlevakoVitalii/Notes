a6d6fc489678a024346b61c2af9c50f3fd7b3423
PlevakoVitalii <plevakovl2102@gmail.com>
2021-06-08 22:27:00 +0300

    add CRUD methods

--- JS/RESful_API.js
+++ JS/RESful_API.js
@@ -45,6 +45,82 @@
 //в npm установим live-server для просмотра результата index.html в браузере
 // в script.js опишем API методы  CRUD
 
-//Код страницы script.js
+//Код страницы script.js 
+
+const baseUrl = 'http://localhost:3000';
+
+const path = {
+  employee: '/employees',
+  comments: '/comments',
+}
+
+// [{key: 'role', value: 'manager'}];
+const generateQueryString = (queryParams = []) => queryParams.length
+  ? `?${queryParams.map(x => `${x.key}=${x.value}`).join(`&`)}`
+  : '';
+
+const getEmployees = async (queryParams) => {
+  const response = await fetch(`${baseUrl}${path.employee}${generateQueryString(queryParams)}`);
+  const items = await response.json();
+
+  const count = Number(response.headers.get('X-Total-Count'));
+
+  return { items, count };
+}
+
+const getEmployee = async (id) => {
+  const response = await fetch(`${baseUrl}${path.employee}/{id}`);
+
+  return employee;
+}
+
+const createEmployee = async (body) => {
+  const response = await fetch (`${baseUrl}${path.employee}`,{
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(body),
+  });
+  const employee = await response.json();
+
+  return employee;
+};
+
+const updateEmployee = async (id, body) => {
+  const response = await fetch (`${baseUrl}${path.employee}/${id}`,{
+    method: 'PUT',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(body),
+  });
+  const employee = await response.json();
+
+  return employee;
+};
+
+const updateEmployeeParam = async (id, body) => {
+  const response = await fetch (`${baseUrl}${path.employee}/${id}`,{
+    method: 'PATCH',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(body),
+  });
+  const employee = await response.json();
+
+  return employee;
+};
+
+//--------
+const main = async () => {
+  const employee = await deleteEmployee(4);
+
+  console.log(employee);
+}
+
+main();
+ 
 
 
